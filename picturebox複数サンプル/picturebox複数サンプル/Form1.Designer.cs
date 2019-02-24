0e3ba4d190c93d575f0ec2a9c812131d8bf2df88
takushi kawata <takushi.kawata@tokai-soft.co.jp>
2019-02-24 19:21:23 +0900

    commit

--- picturebox複数サンプル/picturebox複数サンプル/Form1.Designer.cs
+++ picturebox複数サンプル/picturebox複数サンプル/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +54,33 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "値更新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "表示更新";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 556);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
@@ -70,6 +94,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- picturebox複数サンプル/picturebox複数サンプル/Form1.cs
+++ picturebox複数サンプル/picturebox複数サンプル/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,72 +14,154 @@ namespace picturebox複数サンプル
 {
     public partial class Form1 : Form
     {
+        private Worker[] workers;
         private System.Windows.Forms.PictureBox[] pictureBoxes;
         public Form1()
         {
             InitializeComponent();
 
-            int num = 2;
+            int num = 20;
+            // Worker
+            //var bitmap = new Bitmap(@"test.png");
+            var point = new Point(0, 0);
+            workers = new Worker[num];
+            for (int i = 0; i < workers.Length; i++)
+            {
+                var bitmap = new Bitmap($"worker_image/workers ({(i+1).ToString()}).png");
+                workers[i] = new Worker(i,bitmap, point);
+            }
+
+            // 背景
+            this.pictureBox1.Image = new Bitmap(@"test.png");
+
+            // pictureBox
             this.pictureBoxes = new System.Windows.Forms.PictureBox[num];
             this.SuspendLayout();
 
             this.pictureBoxes[0] = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[0])).BeginInit();
-            this.pictureBoxes[0].Name = "pictureBoxes[0]";
-            this.pictureBoxes[0].Parent = this.pictureBox1;
+            this.pictureBoxes[0].Name = "pictureBoxes0";
+            //this.pictureBoxes[0].Parent = this.pictureBox1;
             this.pictureBoxes[0].BackColor = Color.Transparent;
-            //this.pictureBoxes[0].BackColor = Color.Red;
             this.pictureBoxes[0].Location = new Point(0, 0);
             this.pictureBoxes[0].Size = new Size(pictureBox1.Width, pictureBox1.Height);
             this.pictureBoxes[0].Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-            this.pictureBoxes[0].ImageLocation = @"test.png";
+            this.pictureBoxes[0].Image = workers[0].Bitmap;
             this.Controls.AddRange(this.pictureBoxes);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[0])).EndInit();
             //
-            this.pictureBoxes[1] = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[1])).BeginInit();
-            this.pictureBoxes[1].Name = $"pictureBoxes{1}";
-            this.pictureBoxes[1].Parent = this.pictureBoxes[0];
-            this.pictureBoxes[1].BackColor = Color.Transparent;
-            this.pictureBoxes[1].Location = new Point(0, 0);
-            this.pictureBoxes[1].Size = new Size(pictureBox1.Width, pictureBox1.Height);
-            this.pictureBoxes[1].Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-            this.pictureBoxes[1].ImageLocation = @"test.png";
-            this.Controls.AddRange(this.pictureBoxes);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[1])).EndInit();
-
-
+            for (int i = 1; i < pictureBoxes.Length; i++)
+            {
+                this.pictureBoxes[i] = new System.Windows.Forms.PictureBox();
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[i])).BeginInit();
+                this.pictureBoxes[i].Name = $"pictureBoxes{i}";
+                //this.pictureBoxes[i].Parent = this.pictureBoxes[i - 1];
+                this.pictureBoxes[i].BackColor = Color.Transparent;
+                this.pictureBoxes[i].Location = new Point(0, 0);
+                this.pictureBoxes[i].Size = new Size(pictureBox1.Width, pictureBox1.Height);
+                this.pictureBoxes[i].Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+                this.pictureBoxes[i].Image = workers[i].Bitmap;
+                this.Controls.AddRange(this.pictureBoxes);
+                ((System.ComponentModel.ISupportInitialize)(this.pictureBoxes[i])).EndInit();
+            }
 
             this.ResumeLayout(false);
 
 
-
-
-
-
-            this.pictureBoxes[0].SendToBack();
+            // ピクチャーボックスの順番を変更する
+            for (int i = pictureBoxes.Length - 1; i >= 0; i--)
+            {
+                this.pictureBoxes[i].SendToBack();
+            }
             this.pictureBox1.SendToBack();
+
+            // 親を設定する。
+            this.pictureBoxes[0].Parent = this.pictureBox1;
+            for (int i = 1; i < pictureBoxes.Length; i++)
+            {
+                this.pictureBoxes[i].Parent = this.pictureBoxes[i-1];
+            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.pictureBoxes[1].Invalidate();
-            this.pictureBoxes[1].Update();
-
+            this.pictureBoxes[3].Invalidate();
+            //this.pictureBoxes[1].Update();
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            MessageBox.Show(((System.Windows.Forms.PictureBox)sender).Name);
-            switch (((System.Windows.Forms.PictureBox)sender).Name)
+            if (!Regex.IsMatch(((System.Windows.Forms.PictureBox)sender).Name, "pictureBoxes"))
             {
-                case "pictureBoxes[0]":
-                    break;
-                case "pictureBoxes[1]":
-                    break;
-                default:
-                    break;
+                return;
             }
+            Regex regex = new Regex("pictureBoxes");
+            string result = regex.Replace(((System.Windows.Forms.PictureBox)sender).Name, "");
+            int index = Convert.ToInt32(result);
+            e.Graphics.DrawImage(pictureBoxes[index].Image, workers[index].Point);
+        }
+
+        Random random = new Random();
+        Stopwatch sw = new Stopwatch();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    sw.Reset();
+                    sw.Start();
+                    for (int i = 0; i < workers.Length; i++)
+                    {
+                        workers[i].Point = new Point(random.Next(0, pictureBox1.Width - 1), random.Next(0, pictureBox1.Height - 1));
+                    }
+                    System.Threading.Thread.Sleep(100);
+                    sw.Stop();
+                    Debug.WriteLine(sw.ElapsedMilliseconds);
+                }
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    for (int i = 0; i < pictureBoxes.Length; i++)
+                    {
+                        if (InvokeRequired)
+                        {
+                            Invoke(new Action<int>(update), i);
+                        }
+                        else
+                        {
+                            update(i);
+                        }
+                    }
+                }
+            });
+        }
+
+        void update(int i)
+        {
+            pictureBoxes[i].Invalidate();
+            pictureBoxes[i].Update();
+        }
+    }
+
+    class Worker
+    {
+        public int Id { get; private set; }
+        public Bitmap Bitmap { get;  private set; }
+        public Point Point { get; set; }
+
+        public Worker(int id, Bitmap bitmap, Point point)
+        {
+            this.Id = id;
+            this.Bitmap = bitmap;
+            this.Point = point;
         }
     }
 }
